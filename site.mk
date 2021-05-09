69b05ea6938d350fe85358b6420311041924897c
Dark4MD <github.web@manu.li>
2021-05-09 02:05:52 +0200

    Update site.mk
    GLUON_BRANCH has been deprecated
    set GLUON_AUTOUPDATER_BRANCH and GLUON_AUTOUPDATER_ENABLED instead.

--- site.mk
+++ site.mk
@@ -178,4 +178,5 @@ GLUON_LANGS ?= de en fr
 GLUON_DEPRECATED ?= upgrade
 
 # Set default branch for building custom images
-GLUON_BRANCH ?= experimental
+GLUON_AUTOUPDATER_BRANCH ?= experimental
+GLUON_AUTOUPDATER_ENABLED ?= 1
