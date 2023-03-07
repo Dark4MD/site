e7d798ea5048b7aa82bbca48ddc75ff028d4117c
Dark4MD <github.web@manu.li>
2023-03-07 02:38:39 +0100

    dont build deprecated devices

--- site.mk
+++ site.mk
@@ -175,7 +175,7 @@ GLUON_REGION ?= eu
 GLUON_LANGS ?= de en fr
 
 # Do not build images for deprecated devices
-GLUON_DEPRECATED ?= upgrade
+GLUON_DEPRECATED ?= 0
 
 # Set default branch for building custom images
 GLUON_AUTOUPDATER_BRANCH ?= experimental
